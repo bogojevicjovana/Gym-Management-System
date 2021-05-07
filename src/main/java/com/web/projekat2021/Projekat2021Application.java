9f61c1edc602f4b273955ba0d55ac8fcf07b566a
bogojevicjovana <bogojevic.joc@gmail.com>
2021-05-07 18:05:30 +0200

    first commit

---
+++ src/main/java/com/web/projekat2021/Projekat2021Application.java
@@ -0,0 +1,15 @@
+package com.web.projekat2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekat2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekat2021Application.class, args);
+		System.out.println("Welcome");
+	}
+
+
+}
---
+++ src/test/java/com/web/projekat2021/Projekat2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.projekat2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekat2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
